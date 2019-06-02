eb7dcbe0f3454e0332bc31b6863db76a128ccc6b
hongduclamvt@gmail.com <bitbucket!@#$>
2019-06-02 17:39:04 +0700

    update

---
+++ deploy/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+var proxy = require('express-http-proxy');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, PATCH');
+  return proxy('https://images-api.nasa.gov').apply(null, arguments);
+});
+
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+
