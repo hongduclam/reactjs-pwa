40b3a7e2188eddaf5e53a70ee9412aa22af3a179
hongduclamvt@gmail.com <bitbucket!@#$>
2019-06-04 20:30:38 +0700

    up

--- src/scenes/nasa-collection/components/Item.js
+++ src/scenes/nasa-collection/components/Item.js
@@ -116,6 +116,13 @@ S.ItemImage = styled.div`
 S.ItemAction = styled(FlexDiv)`
 	justify-content: flex-start;
 	margin: 0.5em 0;
+	@media (max-width: 30em) {
+		 span {
+			font-size: 0.8em;
+			padding-left: 0.4em;
+			padding-right: 0;
+		}
+	}
 `;
 
 export const Item = ({ previewImgLink, dateCreated, creator, title, description, actionComponent }) => {
--- src/services/layout/index.js
+++ src/services/layout/index.js
@@ -2,23 +2,35 @@ import _ from "lodash";
 
 // api/loadingReducer.js
 export const loadingReducer = (state = {}, action) => {
-  const { type } = action;
-  const matches = /(.*)_(START|SUCCESS|ERROR)/.exec(type);
+	const { type } = action;
+	const matches = /(.*)_(START|SUCCESS|ERROR)/.exec(type);
 
-  // not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
-  if (!matches) return state;
+	// not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
+	if (!matches) return state;
 
-  const [, requestName, requestState] = matches;
-  return {
-    ...state,
-    // Store whether a request is happening at the moment or not
-    // e.g. will be true when receiving GET_TODOS_REQUEST
-    //      and false when receiving GET_TODOS_SUCCESS / GET_TODOS_FAILURE
-    [requestName]: requestState === "START"
-  };
+	const [, requestName, requestState] = matches;
+	return {
+		...state,
+		// Store whether a request is happening at the moment or not
+		// e.g. will be true when receiving GET_TODOS_REQUEST
+		//      and false when receiving GET_TODOS_SUCCESS / GET_TODOS_FAILURE
+		[requestName]: requestState === "START"
+	};
 };
 
 export const createLoadingSelector = actions => state => {
-  // returns true only when all actions is not loading
-  return _(actions).some(action => _.get(state, `loadingReducer.${action}`));
+	// returns true only when all actions is not loading
+	return _(actions).some(action => _.get(state, `loadingReducer.${action}`));
 };
+
+export function debounce(a, b, c) {
+	var d, e;
+	return function() {
+		function h() {
+			(d = null), c || (e = a.apply(f, g));
+		}
+		var f = this,
+			g = arguments;
+		return clearTimeout(d), (d = setTimeout(h, b)), c && !d && (e = a.apply(f, g)), e;
+	};
+}
